d9f2e891f50b727f76251f929a1263bb1c6a0ccb
linnanjun007-crypto <linnanjun007@gmail.com>
2026-09-07 23:49:03 +0800

    fix: 修正 GitHub App ID

--- src/consts.ts
+++ src/consts.ts
@@ -11,6 +11,6 @@ export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'linnanjun007-crypto',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'blog',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '4861845',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '4862529',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
